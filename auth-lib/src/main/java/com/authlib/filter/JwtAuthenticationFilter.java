9080b2972ec3f075663c5c0af02ebb31f4b5a7c3
Kaustubh Kate <katekaustubh23@gmail.com>
2025-08-25 22:52:17 +0530

    updated auth-lib and inventory service

--- auth-lib/src/main/java/com/authlib/filter/JwtAuthenticationFilter.java
+++ auth-lib/src/main/java/com/authlib/filter/JwtAuthenticationFilter.java
@@ -7,13 +7,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 
-import org.springframework.stereotype.Component;
+//import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 
-@Component
+//@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Value("${internal.secret}")
